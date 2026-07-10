3f31b0f69d36827d42262816c0081dc2a00815c7
Prachi Raghav <prachiraghav.pr@gmail.com>
2026-07-10 19:03:48 +0530

    Add msw config

--- src/test/setup.js
+++ src/test/setup.js
@@ -1,7 +1,13 @@
 import "@testing-library/jest-dom/vitest";
+import { afterEach, beforeAll, afterAll } from "vitest";
 import { cleanup } from "@testing-library/react";
-import { afterEach } from "vitest";
+import { server } from "../mocks/server";
+
+beforeAll(() => server.listen());
 
 afterEach(() => {
   cleanup();
+  server.resetHandlers();
 });
+
+afterAll(() => server.close());
